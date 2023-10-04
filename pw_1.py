590dd22677616cd7a307a8cd99ceb0008554c408
SKY0FF <zaharka2901@gmail.com>
2023-10-04 16:19:04 +0300

    Update pw_1.py

--- pw_1.py
+++ pw_1.py
@@ -1,39 +1,74 @@
 import random
 
-#task 4
+#task 1
 lst_1 = []
 for i in range(10):
     lst_1.append(random.randint(0, 100))
 
-max = lst_1.index(max(lst_1))
-min = lst_1.index(min(lst_1))
+print(lst_1)
+
+max_1 = lst_1.index(max(lst_1))
+min_1 = lst_1.index(min(lst_1))
 
 v = 0
 
-if max > min:
-    while max >= min:
-        v += lst_1[min]
-        min +=1
-elif max < min:
-    while max <= min:
-        v += lst_1[max]
-        max += 1
+if max_1 > min_1:
+    while max_1 >= min_1:
+        v += lst_1[min_1]
+        min_1 +=1
+elif max_1 < min_1:
+    while max_1 <= min_1:
+        v += lst_1[max_1]
+        max_1 += 1
 print(v)
 
 
+#task 2
+
+lst_2 = []
+for i in range(10):
+    lst_2.append(random.randint(0, 100))
+
+print("\n", lst_2)
+
+lst_2.remove(max(lst_2))
+
+print(lst_2)
 
 
+#task 3
 
+lst_3 = []
+for i in range(10):
+    lst_3.append(random.randint(0, 100))
 
+print("\n", lst_3)
 
+lst_3.reverse()
 
+print(lst_3)
 
+lst_4 = []
+for i in range(10):
+    lst_4.append(random.randint(0, 100))
 
+print("\n", lst_4)
 
+for index, value in enumerate(lst_4):
+    print(index, value, end="\n")
+    
+x = 9
 
+for i in range(10):
+    lst_4.append(lst_4[x])
+    x -= 1
 
+for i in range(10):
+    lst_4.remove(lst_4[i-1])
+    
+#for i in range(10):
+#    
+#    lst_4.append(lst_4[i])
+#    lst_4.remove(lst_4[i])
 
-
-#for index, value in enumerate(lst_1):
-#    print(index, value, end="\n")
-
+print(lst_4)
