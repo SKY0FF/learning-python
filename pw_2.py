1dbcc7b6378f27921a0e256ab33fb3edec7fd73d
SKY0FF <zaharka2901@gmail.com>
2023-10-18 15:40:44 +0300

    Update pw_2.py

--- pw_2.py
+++ pw_2.py
@@ -1,17 +1,23 @@
 class Auto:
 
-    def __init__(self, MaxSpeed, Name, BodyType, AmountOfDoors, SizeTrunk):
+    def __init__(self, mofified):
         self.MaxSpeed = 325
         self.Name = "Lamborghini Huracan"
         self.BodyType = "Coupe"
-        self.AmountOdDoors = 2
+        self.AmountOfDoors = 2
         self.SizeTruck = 100
+        self.modified = mofified
     
     def get_year_of_issue(self):
-        return 2014
+        return "Год выпуска - 2014"
     
     def get_acceleration_to_one_hundred(self):
         return 3.2
     
     def get_cylinder_arrangement(self):
         return "V-образный"
+
+car = Auto("True")
+print(car.modified)
+print(car.get_acceleration_to_one_hundred())
+print(car.get_year_of_issue())
