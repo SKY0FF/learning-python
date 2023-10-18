b78a1ce30daf457be1e5574f337175b897460775
SKY0FF <zaharka2901@gmail.com>
2023-10-18 09:30:04 +0300

    Update pw_2.py

--- pw_2.py
+++ pw_2.py
@@ -1,55 +1,17 @@
-import random
+class Auto:
 
-#task 1
-
-lst = []
-for i in range(10):
-    lst.append(random.randint(0, 100))
-
-print(lst)
-
-def is_multiple_three(item):
-    if (item % 3) == 0:
-        return True
-    else:
-        return False
-
-print(list(filter(is_multiple_three, lst)), "\n")
-
-
-#task 2
-lst = []
-for i in range(10):
-    lst.append(random.randint(0, 100))
-
-print(lst)
-
-def minus(item):
-    return item-1
-
-print(list(map(minus, lst)), "\n")
-
-#task 3
-lst = ["Клюшин Захар", "Погосян Ника", "Барабанова Эльвира", "Ванюхин Дмитрий", "Чумаков Максим", "Никулова Анастасия", "Морозова Софья", "Третьяков Антон", "Борисова Ульяна", "Александрук Дмитрий"]
-y = 1 #пока что не зачем
-
-def lower2(item): #делает список с нижним регистром
-    return item.lower()
-
-def split2(item): #делает список с первым словом того списка
-    lst2 = item.split()
-    lst2.remove(lst2[1])
-    return lst2
-
-print(list(map(lower2, lst))) #вывод списка с нижним регистром
-
-lst_2 = list(map(split2, lst)) #создание списка с первым слово списка lst
-print(lst_2) #вывод списка с первым слово списка lst
-
-
-symb = "у" #символ который нужно найти 
-x = 0 #для работы
-for i in range(10):
-    if symb in lst_2[x][0]: #если symb есть в списке списка то вывод да
-        print(x, '-', lst[x])
-    x += 1
+    def __init__(self, MaxSpeed, Name, BodyType, AmountOfDoors, SizeTrunk):
+        self.MaxSpeed = 325
+        self.Name = "Lamborghini Huracan"
+        self.BodyType = "Coupe"
+        self.AmountOdDoors = 2
+        self.SizeTruck = 100
+    
+    def get_year_of_issue(self):
+        return 2014
+    
+    def get_acceleration_to_one_hundred(self):
+        return 3.2
+    
+    def get_cylinder_arrangement(self):
+        return "V-образный"
