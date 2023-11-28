01285a9043778878d9457ed23af7a1f9393405ac
SKY0FF <zaharka2901@gmail.com>
2023-11-29 01:51:41 +0300

    Update lw_1.py

--- lw_1.py
+++ lw_1.py
@@ -1,3 +1,4 @@
+#task 1
 class List():
     class Item():
         def __init__(self, value, next_item=None):
@@ -5,43 +6,50 @@ class List():
             self.next_item = next_item
         
     def __init__(self):
-        self.first_time = None
+        self.first_item = None
         self.count = 0
+    
+    def add_first_time(self, value):
+        self.first_item = self.Item(value)
+        self.count += 1
 
     def append(self, value):
         if self.count == 0:
-            self.first_time = self.Item(value)
+            self.add_first_time(value)
         else:
-            item = self.first_time
+            item = self.first_item
             while item.next_item:
                 item = item.next_item
             item.next_item = self.Item(value)
-        self.count += 1
+            self.count += 1
     
     def insert(self, value, index):
         if self.count == 0:
-            self.first_time = self.Item(value)
-            self.count += 1
+            self.add_first_time(value)
         else:
-            item = self.first_time
+            item = self.first_item
             x = 0
             if x == index:
-                item.value = value
-            elif self.count <= index:
-                while item.next_item:
-                    item = item.next_item
-                item.next_item = self.Item(value)
+                item2 = self.first_item
+                self.first_item = self.Item(value, item2)
                 self.count += 1
+            elif self.count <= index:
+                self.append(value)
             else:
                 while x != index:
-                    x += 1
-                    item = item.next_item
-                item.value = value
+                    if x+1 == index:
+                        break
+                    else:
+                        x += 1
+                        item = item.next_item
+                item2 = item.next_item
+                item.next_item = self.Item(value, item2)
+                self.count += 1
 
     def remove(self, value):
-        item = self.first_time
+        item = self.first_item
         if item.value == value:
-            self.first_time = item.next_item
+            self.first_item = item.next_item
         while item.value != value:
             if item.next_item.value == value:
                 break
@@ -52,7 +60,7 @@ class List():
         self.count -= 1
 
     def index(self, value):
-        item = self.first_time
+        item = self.first_item
         if item.value == value:
             return 0
         x = 0
@@ -66,7 +74,7 @@ class List():
             return var
         else:
             x = ""
-            item = self.first_time
+            item = self.first_item
             for i in range(self.count):
                 if item.next_item:
                     x += f"{item.value}, "
@@ -74,7 +82,8 @@ class List():
                 else:
                     x += f"{item.value}"
             return f"[{x}]"
-    
+
+print("Задание №1")
 lst = List()
 lst.append(9)
 lst.append(True)
@@ -84,6 +93,7 @@ print(lst, "\n")
 lst.insert("IT-cube", 5)
 lst.append(2023)
 lst.remove(9)
+lst.append(2007)
 
 print(lst, "\n")
 
@@ -91,4 +101,43 @@ lst.insert("IT-circle", 1)
 
 print(lst, "\n")
 
-print(lst.index(2023))
+print(f"{lst.index(2023)}\n")
+
+#task2
+def sort(list, start, end):
+    if end - start > 1:
+        mid = (start + end)//2
+        sort(list, start, mid)
+        sort(list, mid, end)
+        lst_sort(list, start, mid, end)
+ 
+def lst_sort(list, start, mid, end):
+    left = list[start:mid]
+    right = list[mid:end]
+    k = start
+    i = 0
+    j = 0
+    while (start + i < mid and mid + j < end):
+        if (left[i] <= right[j]):
+            list[k] = left[i]
+            i = i + 1
+        else:
+            list[k] = right[j]
+            j = j + 1
+        k = k + 1
+    if start + i < mid:
+        while k < end:
+            list[k] = left[i]
+            i = i + 1
+            k = k + 1
+    else:
+        while k < end:
+            list[k] = right[j]
+            j = j + 1
+            k = k + 1
+ 
+print("Задание №2")
+lst = [1, 2, 7, 123, 93213, 478, 5, -123, 0]
+print(f"Список до сортировки: {lst}")
+sort(lst, 0, len(lst))
+print(f"Отсортированный список: {lst}")
