85e718fa810dedfdf5e8a2f0be5757fd14091d84
SKY0FF <zaharka2901@gmail.com>
2023-11-29 01:10:43 +0300

    Update pw_2.py

--- pw_2.py
+++ pw_2.py
@@ -1,24 +1,24 @@
 class Auto:
 
-    def __init__(self, Name, MaxSpeed, BodyType, AmountOfDoors, SizeTruck, modified, year):
-        self.MaxSpeed = MaxSpeed
-        self.Name = Name
-        self.BodyType = BodyType
-        self.AmountOfDoors = AmountOfDoors
-        self.SizeTruck = SizeTruck
+    def __init__(self, name, max_speed, body_type, amount_of_doors, size_truck, modified, year):
+        self.max_speed = max_speed
+        self.name = name
+        self.body_type = body_type
+        self.amount_of_doors = amount_of_doors
+        self.size_truck = size_truck
         self.modified = modified
         self.year = year
     
     def get_year(self):
-        return "Год поставки - " + str(self.year)
+        return f"Год поставки - {self.year}"
     
-    def get_mod(self):
-        if self.modified == "Yes":
+    def is_modified(self):
+        if self.modified:
             return "Машина модифицированна"
-        elif self.modified == "No":
+        else:
             return "Машина не модифицированна"
 
-car = Auto("Lamborghini Huracan", 325, "Coupe", 2, 100, "Yes", 2023)
-print(car.MaxSpeed)
-print(car.get_mod())
+car = Auto("Lamborghini Huracan", 325, "Coupe", 2, 100, True, 2023)
+print(car.max_speed)
+print(car.is_modified())
 print(car.get_year())
