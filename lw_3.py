6041769250306f5fcf71b420011adfc1a03a92ce
SKY0FF <104008419+SKY0FF@users.noreply.github.com>
2023-11-29 18:33:14 +0300

    Create lw_3.py

---
+++ lw_3.py
@@ -0,0 +1,13 @@
+class MyGraf():
+    def __init__(self, lst_vertex, lst_edge, lst_values):#lst_verte - строчный список с одним символом в элементе, lst_edge - строчный список с ребрами, lst_values - такой длина как ребра, и соответствует положениюю ребер  ВСЁ С КЛАВИАТУРЫ
+        self.lst_vertex = lst_vertex
+        self.lst_edge = lst_edge
+        self.lst_values = lst_values
+        self.adjacency_matrix #тут матрица смежности -> в поле класса
+
+    def deykster(self):
+        pass
+    
+    def __str__(self):
+        #вывод матрицы смежности
+        pass
