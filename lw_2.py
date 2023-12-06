850c6986ed446da6aed418b0d309952baf74155b
SKY0FF <zaharka2901@gmail.com>
2023-12-06 15:28:59 +0300

    Update lw_2.py

--- lw_2.py
+++ lw_2.py
@@ -17,11 +17,35 @@ import math
 #print(get_solution(a, b, c))
 
 #task2
-tpl = (1, "str", 1023, 9, True, 123, False)
-item = int(input("Введите элемент: "))
-def get_part_of_tuple(tpl, item):
-    return tpl[tpl.index(item)+1], tpl[tpl.index(item)+2], tpl[tpl.index(item)+3]
-
-print(get_part_of_tuple(tpl, item))
+#tpl = tuple(input("Введите кортеж, между элементами поставьте пробел: ").split())
+#item = input("Введите элемент: ")
+#def get_part_of_tuple(tpl, item):
+#    tpl = list(map(str, tpl))
+#    x = []
+#    if (tpl.index(item)+1) < len(tpl):
+#        x.append(tpl[tpl.index(item)+1])
+#    else:
+#        x = ["Следующих элементов в кортеже нет!"]
+#    if (tpl.index(item)+2) < len(tpl):
+#        x.append(tpl[tpl.index(item)+2])
+#    if (tpl.index(item)+3) < len(tpl):
+#        x.append(tpl[tpl.index(item)+3])
+#    return tuple(x)
+#
+#print(get_part_of_tuple(tpl, item))
 
 #task3
+#item = set(input("Введите число, которое нужно проверить, между цифрами в числе поставьте пробел: ").split())
+#def is_true_number(item):
+#    st = set({"0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B"})
+#    if st.issuperset(item):
+#        return "Число записано корректно!"
+#    else:
+#        return "Число записано некорректно!"
+#print(is_true_number(item))
+
+#task4
+lst = list(input("Введите слова, между ними поставьте пробел: ").split())
+st = set(lst)
+print(lst)
+print(st)
