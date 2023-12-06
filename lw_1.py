9688b80108b6070b3cd9cc51eb9db03e5d9d16d6
SKY0FF <zaharka2901@gmail.com>
2023-12-06 15:52:34 +0300

    lw_1

--- lw_1.py
+++ lw_1.py
@@ -1,3 +1,4 @@
+#task 1
 class List():
     class Item():
         def __init__(self, value, next_item=None):
@@ -100,3 +101,40 @@ lst.insert("IT-circle", 1)
 print(lst, "\n")
 
 print(f"{lst.index(2023)}\n")
+
+#task 2
+def sort(lst):
+    if len(lst)>1:
+        mid = len(lst)//2
+        lefthalf = lst[:mid]
+        righthalf = lst[mid:]
+
+        sort(lefthalf)
+        sort(righthalf)
+
+        i = 0
+        j = 0
+        k = 0
+        while i<len(lefthalf) and j<len(righthalf):
+            if lefthalf[i]<righthalf[j]:
+                lst[k]=lefthalf[i]
+                i=i+1
+            else:
+                lst[k]=righthalf[j]
+                j += 1
+            k += 1
+
+        while i<len(lefthalf):
+            lst[k]=lefthalf[i]
+            i += 1
+            k += 1
+
+        while j<len(righthalf):
+            lst[k]=righthalf[j]
+            j += 1
+            k += 1
+
+lst = [54,26,93,17,77,31,44,55,20]
+print(f"Список до сортировки: {lst}")
+sort(lst)
+print(f"Список после сортировки: {lst}")
