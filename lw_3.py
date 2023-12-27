70c52eda9bd4894d202cc4f2450c4b7d913a52a8
SKY0FF <zaharka2901@gmail.com>
2023-12-27 16:33:03 +0300

    Update lw_3.py

--- lw_3.py
+++ lw_3.py
@@ -4,8 +4,22 @@ class MyGraf():
         self.lst_edge = lst_edge
         self.lst_values = lst_values
         self.adjacency_matrix = []
-        for i in range(len(lst_vertex)):
+        for i in range(len(lst_vertex)):#заполенение нулями матрицы
             self.adjacency_matrix.append([0]*len(lst_vertex))
+        
+        self.lst_edge_1 = []
+        self.lst_edge_2 = []
+        for i in range(len(lst_edge)):#вынос в отдельные списки первое и второе значения ребра
+            self.lst_edge_1.append((self.lst_edge[i])[0])
+            self.lst_edge_2.append((self.lst_edge[i])[1])
+    
+
+        for j in range(len(lst_vertex)):
+            if lst_vertex[0] in self.lst_edge_1[j]:
+                self.adjacency_matrix[0][j] = 1
+        #for i in range(len(self.adjacency_matrix)): 
+        #    for j in range(len(self.adjacency_matrix)):
+        #        self.adjacency_matrix[i][j] = j
         #тут матрица смежности -> в поле класса
 
     def deykster(self):
@@ -17,7 +31,7 @@ class MyGraf():
             for j in range(len(self.adjacency_matrix)):
                 x += f"{self.adjacency_matrix[i][j]}"
             x += '\n'
-        return x
+        return f"{x}{self.lst_edge_1}\n{self.lst_edge_2}\n{self.lst_all}"
         #вывод матрицы смежности
 
 graf = MyGraf(["A", "B", "C", "D"], ["AC", "AD", "AB", "CB"], [5, 6, 6, 9])
