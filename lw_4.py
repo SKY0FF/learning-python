375a958a331c6c4fc264c3aa8d83be1ee1e3d34e
SKY0FF <104008419+SKY0FF@users.noreply.github.com>
2024-01-17 19:05:33 +0300

    Add files via upload

---
+++ lw_4.py
@@ -0,0 +1,17 @@
+import hashlib
+
+data = '123'
+print(hash(data))
+#class Dict(): 
+#     
+#    def __init__(self): 
+#        self.__size = 25 
+#        self.__data = [[],] * self.__size#список в списке там значение, а хэш который генериться его индексы
+#    
+#    def add(self):
+#        #тут на вход значение, к нему вычисляется хэш и сохраняется в словарь
+#        pass
+#
+#    def get(self):
+#        #тут вывод по ключу вывод значения
+#        pass
