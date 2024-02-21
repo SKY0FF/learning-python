266a3ebbe94d48b14b8a28ea66da9823974a972e
SKY0FF <104008419+SKY0FF@users.noreply.github.com>
2024-02-21 18:27:54 +0300

    Update pw_3.py

--- pw_3.py
+++ pw_3.py
@@ -1,11 +1,24 @@
 class Vehicle:
 
-    def __init__(self):
-        pass
-    pass
+    def __init__(self, brand, speed):
+        self.brand = brand
+        self.speed = speed
+
+    def move(self):
+        print('Это транспортное средство перемещается')
 
 class Auto(Vehicle):
-    pass
+
+    def __init__(self, body_type):
+        self.body_type = body_type
+
+    def ride(self):
+        print("Это транспортное средство ездит")
 
 class Airplane(Vehicle):
-    pass
+
+    def __init__(self, airplane_type):
+        self.type = airplane_type
+        
+    def fly(self):
+        print("Это транспортное средство летает")
